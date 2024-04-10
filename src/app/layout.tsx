523d4fbf48c2e5e3f98b97ff027fc3a9f7ac2f4e
yolada.c <yolada.c@greenmount.co.th>
2024-04-10 16:46:58 +0700

    font googleapis
    Signed-off-by: yolada.c <yolada.c@greenmount.co.th>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,17 @@ import "./globals.css";
 
 import Head from "next/head";
 import React, { useState } from "react";
-import { Noto_Sans_Thai } from "next/font/google";
+import { Noto_Sans_Thai, Sarabun } from "next/font/google";
 //👇 Configure our font object
 const noto = Noto_Sans_Thai({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  subsets: ["latin", "thai"],
+  subsets: ["thai", "latin"],
+  display: "swap",
+  variable: "--font-noto",
+});
+const sarabun = Sarabun({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  subsets: ["thai", "latin"],
   display: "swap",
   variable: "--font-noto",
 });
@@ -17,13 +23,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="th">
       <Head>
         {/* preconnect scripts... */}
         <title>xchat</title>
         <link rel="icon" href="../asset/logo/logo.png"></link>
       </Head>
-      <body className={noto.className}>{children}</body>
+
+      <body className={sarabun.className}>{children}</body>
     </html>
   );
 }
